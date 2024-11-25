e93844d5978938b2c547fc9c6fd7695c1f817b1e
Warlock <warlockbest@yandex.ru>
2024-11-25 12:16:42 +0300

    update kernel

--- tools/irbpkg/PKG.java
+++ tools/irbpkg/PKG.java
@@ -10,10 +10,12 @@ import java.util.List;
 class PKG
 {
 	private final String pkgname;
+    public boolean exist = false;
 
     public PKG(String pkg_name)
     {
         this.pkgname = pkg_name;
+        this.exist = check_installed_pkg();
     }
     
     public boolean find_pkg(String path)
@@ -22,17 +24,23 @@ class PKG
         return !Files.exists(path__dir_pkg);
     }
     
-    public List<String> check_depends()
+    public List<PKG> check_depends()
     {
-        List<String> deps_list = new ArrayList<>();
+        List<PKG> deps_list = new ArrayList<>();
         String depP = find_line_from_file().substring(pkgname.length()+1);
         String[] dep = depP.split("\\s+");
         for (String s : dep) {
             if (!s.isEmpty()) {
-                deps_list.add(s);
+                PKG x = new PKG(s);
+                deps_list.add(x);
             }
         }
  	    return deps_list;
+    }
+
+    public String getPkgname()
+    {
+        return pkgname;
     }
 
     private String find_line_from_file()
@@ -55,5 +63,13 @@ class PKG
 
     private List<String> readTextFile(String pathx) throws IOException {
         return Files.readAllLines(Paths.get(pathx));
+    }
+
+    //  /var/lib/dpkg/info/curl.list  папка содержит файлы пакетов если файл существует пакет установленн
+    public boolean check_installed_pkg()
+    {
+        String path = "/var/lib/dpkg/info/"+this.pkgname+".list";
+        Path path__dir_pkg = Paths.get(path);
+        return Files.exists(path__dir_pkg);
     }
 }
--- tools/irbpkg/irbpkg.java
+++ tools/irbpkg/irbpkg.java
@@ -1,5 +1,8 @@
 package su.irbis.irbpkg;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class irbpkg {
@@ -17,14 +20,25 @@ public class irbpkg {
         String namepkg = args[0];
         System.out.println("IRBIS PKG Manager ");
         PKG pkg = new PKG(namepkg);
-        if (pkg.find_pkg(pkg_dir))
+        if (find_pkg(pkg_dir))
         {
             System.out.println("Pkg file not fount "+namepkg);
         }
-        List<String> depends =  pkg.check_depends();
-        irbpkg.Debug("depends " + depends + " count "+ depends.size());
+        //Получение списка зависимостей
+        List<PKG> depends =  pkg.check_depends();
+        //irbpkg.Debug("depends " + depends + " count "+ depends.size());
+        //Проверка установленных зависимостей
+        boolean depFail = false;
+        for (PKG dep : depends)
+        {
+            if (!dep.exist)
+            {
+                Debug("pakage not installed " + dep.getPkgname() );
+                depFail = true;
+            }
+        }
 
-
+        if(depFail) { return; }
     }
 
     private static void usage()
@@ -33,9 +47,14 @@ public class irbpkg {
         System.out.println("ipkg options pkg_name ");
     }
 
+    private static boolean find_pkg(String path)
+    {
+        Path path__dir_pkg = Paths.get(path);
+        return !Files.exists(path__dir_pkg);
+    }
+
     public static void Debug(String text)
     {
         System.out.println(text);
     }
 }
-
